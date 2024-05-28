bee92a8c4435db9b497360731d71d28f076e0377
Alexander Angelidis <alex.m.angelidis@gmail.com>
2024-05-28 13:59:29 +0100

    nearest neigbour soln

--- toplevel.cpp
+++ toplevel.cpp
@@ -5,6 +5,9 @@
 #include <cstdint>
 #include <cstring> // Include the <cstring> header for memcpy
 
+#include <vector>
+#define INF 0xFF
+
 // Main Function
 int listener(uint32 *ram, uint32 *message_id, uint32 *number_of_cities, uint32 *scenario_id) {
 	#pragma HLS INTERFACE m_axi port=ram offset=slave bundle=MAXI
@@ -27,6 +30,68 @@ int listener(uint32 *ram, uint32 *message_id, uint32 *number_of_cities, uint32 *
 
 int index(int row, int col, int total_cities) {
 	return row + total_cities * col;
+}
+
+int nearest_neigbour_first (uint8 adjacency_matrix[], int num_cities) {
+    int run = 1;
+    std::vector<int> visited_cities;
+    
+    int distance = INF;
+    int city_pointer = 0;
+    int min_distance = INF;
+    int min_city = -1;
+    
+    int skip = 0;
+    int worst_case_distance = INF;
+    
+    // start at city 0
+    visited_cities.push_back(0);
+
+    while (run) {
+
+        // get the last city visited
+        for (int i = 0; i < num_cities; i++) {
+            skip = 0;
+            city_pointer = visited_cities.back();
+            distance = adjacency_matrix[index(city_pointer, i, num_cities)];
+            
+            if (distance == 0) { continue; }
+
+            for (int j = 0; j < visited_cities.size(); j++) {
+                if (visited_cities[j] == i) {
+                    skip = 1;
+                }
+            }
+
+            if (skip) { continue; }
+
+            if (distance < min_distance) {
+                min_distance = distance;
+                min_city = i;
+            }
+        }
+        
+
+        if (visited_cities.size() == num_cities) {
+            run = 0;
+        } else {
+            // printf("City: %d, Distance: %d\n", min_city, min_distance); 
+            visited_cities.push_back(min_city);
+            worst_case_distance += min_distance;
+            min_distance = INF;
+        }
+    }
+
+    // finish with a loop back to city 0
+    worst_case_distance += adjacency_matrix[index(visited_cities.back(), 0, num_cities)];
+
+    // print out the visited_cities vector as this contains your nearest neigbour tour
+    for (int i = 0; i < visited_cities.size(); i++) { printf("%d ", visited_cities[i]); }
+
+    printf("\n");
+    printf("Your upper bound for distance is %d", worst_case_distance);
+    
+    return 0;
 }
 
 // Main Function - solver
@@ -60,9 +125,12 @@ int toplevel(uint32 *ram, uint32 *message_id, uint32 *number_of_cities, uint32 *
 		}
 		printf("\n");
 	}
+    printf("\n");
 
 	// Rest of the code...
     // Nearest neigbour first soln
+    nearest_neigbour_first(cache, int_number_of_cities);
+
 
 	// Finish up - add the shortest path
 	params.shortest_path = shortest_path;
