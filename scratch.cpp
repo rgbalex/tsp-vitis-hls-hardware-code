f94cc2027ceb827319bd5add8aca5371fb785af6
Alexander Angelidis <alex.m.angelidis@gmail.com>
2024-05-28 12:55:15 +0100

    Second scratch

--- scratch.cpp
+++ scratch.cpp
@@ -1,85 +1,106 @@
-// A simple C++ implementation to find minimum 
-// spanning tree for adjacency representation.
+// A C++ program for Prim's Minimum
+// Spanning Tree (MST) algorithm. The program is
+// for adjacency matrix representation of the graph
+
 #include <bits/stdc++.h>
 using namespace std;
-#define V 4
 
-// Returns true if edge u-v is a valid edge to be
-// include in MST. An edge is valid if one end is
-// already included in MST and other is not in MST.
-bool isValidEdge(int u, int v, vector<bool> inMST)
+// Number of vertices in the graph
+#define V 5
+
+// A utility function to find the vertex with
+// minimum key value, from the set of vertices
+// not yet included in MST
+int minKey(int key[], bool mstSet[])
 {
-if (u == v)
-	return false;
-if (inMST[u] == false && inMST[v] == false)
-		return false;
-else if (inMST[u] == true && inMST[v] == true)
-		return false;
-return true;
+	// Initialize min value
+	int min = INT_MAX, min_index;
+
+	for (int v = 0; v < V; v++)
+		if (mstSet[v] == false && key[v] < min)
+			min = key[v], min_index = v;
+
+	return min_index;
 }
 
-void primMST(int cost[][V])
-{ 
-	vector<bool> inMST(V, false);
+// A utility function to print the
+// constructed MST stored in parent[]
+void printMST(int parent[], int graph[V][V])
+{
+	cout << "Edge \tWeight\n";
+	for (int i = 1; i < V; i++)
+		cout << parent[i] << " - " << i << " \t"
+			<< graph[i][parent[i]] << " \n";
+}
 
-	// Include first vertex in MST
-	inMST[0] = true;
+// Function to construct and print MST for
+// a graph represented using adjacency
+// matrix representation
+void primMST(int graph[V][V])
+{
+	// Array to store constructed MST
+	int parent[V];
 
-	// Keep adding edges while number of included
-	// edges does not become V-1.
-	int edge_count = 0, mincost = 0;
-	while (edge_count < V - 1) {
+	// Key values used to pick minimum weight edge in cut
+	int key[V];
 
-		// Find minimum weight valid edge. 
-		int min = INT_MAX, a = -1, b = -1;
-		for (int i = 0; i < V; i++) {
-			for (int j = 0; j < V; j++) {			 
-				if (cost[i][j] < min) {
-					if (isValidEdge(i, j, inMST)) {
-						min = cost[i][j];
-						a = i;
-						b = j;
-					}
-				}
-			}
-		}
-		if (a != -1 && b != -1) {
-			printf("Edge %d:(%d, %d) cost: %d \n", 
-						edge_count++, a, b, min);
-			mincost = mincost + min;
-			inMST[b] = inMST[a] = true;
-		}
+	// To represent set of vertices included in MST
+	bool mstSet[V];
+
+	// Initialize all keys as INFINITE
+	for (int i = 0; i < V; i++)
+		key[i] = INT_MAX, mstSet[i] = false;
+
+	// Always include first 1st vertex in MST.
+	// Make key 0 so that this vertex is picked as first
+	// vertex.
+	key[0] = 0;
+
+	// First node is always root of MST
+	parent[0] = -1;
+
+	// The MST will have V vertices
+	for (int count = 0; count < V - 1; count++) {
+		
+		// Pick the minimum key vertex from the
+		// set of vertices not yet included in MST
+		int u = minKey(key, mstSet);
+
+		// Add the picked vertex to the MST Set
+		mstSet[u] = true;
+
+		// Update key value and parent index of
+		// the adjacent vertices of the picked vertex.
+		// Consider only those vertices which are not
+		// yet included in MST
+		for (int v = 0; v < V; v++)
+
+			// graph[u][v] is non zero only for adjacent
+			// vertices of m mstSet[v] is false for vertices
+			// not yet included in MST Update the key only
+			// if graph[u][v] is smaller than key[v]
+			if (graph[u][v] && mstSet[v] == false
+				&& graph[u][v] < key[v])
+				parent[v] = u, key[v] = graph[u][v];
 	}
-	printf("\n Minimum cost= %d \n", mincost);
+
+	// Print the constructed MST
+	printMST(parent, graph);
 }
 
-// driver program to test above function
+// Driver's code
 int main()
 {
-	/* Let us create the following graph
-		2 3
-	(0)--(1)--(2)
-	| / \ |
-	6| 8/ \5 |7
-	| /	 \ |
-	(3)-------(4)
-			9		 */
-	// int cost[][V] = {
-	// 	{ INT_MAX, 2, INT_MAX, 6, INT_MAX },
-	// 	{ 2, INT_MAX, 3, 8, 5 },
-	// 	{ INT_MAX, 3, INT_MAX, INT_MAX, 7 },
-	// 	{ 6, 8, INT_MAX, INT_MAX, 9 },
-	// 	{ INT_MAX, 5, 7, 9, INT_MAX },
-	// };
-    int cost[][V] = {
-        { 0, 177, 252, 18 },
-        { 177, 0, 73, 250 },
-        { 252, 73, 0, 189 },
-        { 18, 250, 189, 0 },
-    };  
+	int graph[V][V] = { { 0, 2, 0, 6, 0 },
+						{ 2, 0, 3, 8, 5 },
+						{ 0, 3, 0, 0, 7 },
+						{ 6, 8, 0, 0, 9 },
+						{ 0, 5, 7, 9, 0 } };
 
 	// Print the solution
-	primMST(cost);
+	primMST(graph);
 
 	return 0;
 }
+
+// This code is contributed by rathbhupendra
